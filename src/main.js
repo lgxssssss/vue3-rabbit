acf7e5d952e15d6ee5b71ab05f43281640afe533
lgxssssss <127862134+lgxssssss@users.noreply.github.com>
2024-08-07 18:42:08 +0800

    静态资源的引入和创建

--- src/main.js
+++ src/main.js
@@ -6,13 +6,15 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+//引入初始化样式文件
+import '@/styles/common.scss'
+
 //测试接口函数
 import { getCategory } from './apis/testAPI'
 getCategory().then(res => {
     console.log(res);
     
 })
-
 
 const app = createApp(App)
 
