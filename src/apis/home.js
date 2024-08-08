e3027349aee900f28193d19edc47f31ade2e3e53
lgxssssss <127862134+lgxssssss@users.noreply.github.com>
2024-08-08 20:50:11 +0800

    product列表渲染

--- src/apis/home.js
+++ src/apis/home.js
@@ -17,8 +17,25 @@ export const findNewAPI = () => {
       url: '/home/new'
     })
   }
+
+  /**
+ * @description: 获取人气推荐
+ * @param {*}
+ * @return {*}
+ */
 export const findHotAPI = () => {
     return httpInstance({
       url: '/home/hot'
     })
   }
+
+  /**
+ * @description: 获取所有商品模块
+ * @param {*}
+ * @return {*}
+ */
+export const getGoodsAPI = () => {
+  return httpInstance({
+    url: '/home/goods'
+  })
+}
