98c9610a2da0fd45e86c1f2416e35509ce8c09a8
lgxssssss <127862134+lgxssssss@users.noreply.github.com>
2024-08-08 16:50:31 +0800

    整体的架构拆分，分类组件的渲染

--- src/views/Layout/index.vue
+++ src/views/Layout/index.vue
@@ -6,7 +6,7 @@ import LayoutFooter from './components/LayoutFooter.vue'
 import LayoutFixed from './components/LayoutFixed.vue'
 
 // 触发获取导航列表的action
-//很关键，之后再触发请求获取数据之后所有的Layout里面的组件都能够使用
+//很关键，之后再触发请求获取数据之后所有组件都能够使用
 import { useCategoryStore } from '@/stores/categoryStore'
 import { onMounted } from 'vue'
 
