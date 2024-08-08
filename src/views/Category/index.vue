6690c3fe18f8df55c68a5bbfd81be1408e117ddb
lgxssssss <127862134+lgxssssss@users.noreply.github.com>
2024-08-08 22:45:50 +0800

    一级激活状态显示以及费雷列表渲染

--- src/views/Category/index.vue
+++ src/views/Category/index.vue
@@ -4,6 +4,7 @@ import { getCategoryAPI } from'@/apis/category'
 import { onMounted, ref } from 'vue';
 import { useRoute } from 'vue-router';
 import { getBannerAPI } from '@/apis/home'
+import GoodsItem from '@/views/Home/components/GoodsItem.vue';
 
 const route = useRoute()
 const categoryData = ref({})
@@ -43,7 +44,26 @@ onMounted(() => getBanner())
             <img :src="item.imgUrl" alt="">
           </el-carousel-item>
         </el-carousel>
-  </div>
+      </div>
+      <div class="sub-list">
+        <h3>全部分类</h3>
+        <ul>
+          <li v-for="i in categoryData.children" :key="i.id">
+            <RouterLink :to="`/category/sub/${i.id}`">
+              <img :src="i.picture" />
+              <p>{{ i.name }}</p>
+            </RouterLink>
+          </li>
+        </ul>
+      </div>
+      <div class="ref-goods" v-for="item in categoryData.children" :key="item.id">
+        <div class="head">
+          <h3>- {{ item.name }}-</h3>
+        </div>
+        <div class="body">
+          <GoodsItem v-for="good in item.goods" :goods="good" :key="good.id" />
+        </div>
+      </div>
     </div>
   </div>
 </template>
