b2c725f2bc82b6769932d229d008c95d3a3e3c6b
lgxssssss <127862134+lgxssssss@users.noreply.github.com>
2024-08-11 20:14:09 +0800

    401拦截处 理

--- src/utils/http.js
+++ src/utils/http.js
@@ -1,6 +1,7 @@
 // axios基础的封装
 import axios from 'axios'
 import { useUserStore } from '@/stores/userStore'
+import router from '@/router'
 const httpInstance = axios.create({
   baseURL: 'http://pcapi-xiaotuxian-front-devtest.itheima.net',
   timeout: 5000
@@ -20,11 +21,16 @@ httpInstance.interceptors.request.use(config => {
 
 // axios响应式拦截器
 httpInstance.interceptors.response.use(res => res.data, e => {
+  const userStore = useUserStore()
   // 处理业务失败, 给错误提示，抛出错误
   ElMessage({
     type: 'warning',
     message: e.response.data.message
   })
+  if (e.response.status === 401) {
+    userStore.delUserInfo()
+    router.push('/login')
+  }
   return Promise.reject(e)
 })
 
