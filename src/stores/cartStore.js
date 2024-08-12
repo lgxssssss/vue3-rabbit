e67ffa93369e7b7fe3dbb62881f1881a7c5fbd91
lgxssssss <127862134+lgxssssss@users.noreply.github.com>
2024-08-12 23:27:01 +0800

    本地购物车-购物车列表-全选功能

--- src/stores/cartStore.js
+++ src/stores/cartStore.js
@@ -22,9 +22,16 @@ export const useCartStore = defineStore('cart', () => {
       cartList.value.splice(idx, 1)
   }
 
+  // 单选功能
   const changeSelect = (skuId, selected) => {
     const item = cartList.value.find((item) => item.skuId === skuId)
     item.selected = selected 
+  }
+
+  // 全选功能
+  const allCheck = (selected) => {
+    // 把cartList中的每一项的selected都设置为当前的全选框状态
+    cartList.value.forEach(item => item.selected = selected)
   }
 
    // 计算属性
@@ -33,13 +40,18 @@ export const useCartStore = defineStore('cart', () => {
   // 2. 总价 所有项的count*price之和
   const allPrice = computed(() => cartList.value.reduce((a, c) => a + c.count * c.price, 0))
 
+    // 是否全选
+    const isAll = computed(() => cartList.value.every((item) => item.selected))
+
     return {
         cartList,
-        addCart,
-        delCart,
         allCount,
         allPrice,
-        changeSelect
+        isAll,
+        addCart,
+        delCart,
+        changeSelect,
+        allCheck
     }
 },{
     persist:true
