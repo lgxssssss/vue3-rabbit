e04c3a1d507981804caaa3897c165b7e51ffded3
lgxssssss <127862134+lgxssssss@users.noreply.github.com>
2024-08-14 00:21:13 +0800

    结算 -提交订单业务实现

--- src/apis/checkout.js
+++ src/apis/checkout.js
@@ -10,10 +10,10 @@ export const getCheckInfoAPI = () => {
 
 
 // 创建订单
-// export const createOrderAPI = (data) => {
-//   return request({
-//     url: '/member/order',
-//     method: 'POST',
-//     data
-//   })
-// }
+export const createOrderAPI = (data) => {
+  return request({
+    url: '/member/order',
+    method: 'POST',
+    data
+  })
+}
